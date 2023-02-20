01cf3f7e1cf21f570a985559c4d321b5f0b12112
Cranky Bawler <6666lenia6666@mail.ru>
2023-02-20 20:11:47 +0300

    Задача №8-9

--- src/Main.java
+++ src/Main.java
@@ -90,7 +90,7 @@ public class Main {
         for (int i = 0; i < arr.length - 1; i++)
             for (int j = 0; j < arr.length - 1; j++) {
                 if (arr[i] + arr[j] == -2) {
-                    System.out.println(arr[i] + " , " + arr[j]);
+                System.out.println(arr[i] + " , " + arr[j]);
                 }
             }
     }
